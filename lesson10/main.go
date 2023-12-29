b719921d042c17236540cc8225edbefc219fef68
liuxian <2929504046@qq.com>
2023-12-29 16:11:00 +0800

    add: a simple of CI/CD form Github Action

--- lesson10/main.go
+++ lesson10/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	// 创建一个Gin路由器
+	router := gin.Default()
+
+	// 创建一个路由处理器
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// 启动HTTP服务器并监听端口
+	router.Run(":8080")
 }
